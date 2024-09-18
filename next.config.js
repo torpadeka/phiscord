2f2ecb03814a39c9911f843b5abcc1da91c28005
BuildTools <unconfigured@null.spigotmc.org>
2024-09-19 00:09:15 +0700

    Re-setup more configs

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
-  // output: 'export',
-  distDir: process.env.NODE_ENV === 'production' ? '../app' : '.next',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config) => {
-    return config
-  },
-}
+    reactStrictMode: true,
+};
